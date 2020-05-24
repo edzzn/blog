5923a4a5c5c601c5c289d4cc1b3feef8a737945f
Edisson Reinozo <edisson.reinozo@ucuenca.edu.ec>
2020-05-24 12:53:45 -0500

    Add Category, Tags & Description

--- src/components/home/components/RecerArticles.js
+++ src/components/home/components/RecerArticles.js
@@ -8,13 +8,25 @@ const RecentArticles = ({ articles }) => (
   <section>
     <h2>Artículos Recientes</h2>
     {/* <Link>Ver todos</Link> */}
-    <div className='grid grid-cols-12 gap-4'>
+    <div className='grid grid-cols-12 gap-20'>
       {articles.map((article) => (
-        <div key={article.slug} className='col-span-12 sm:col-span-6'>
+        <div key={article.slug} className='col-span-12 sm:col-span-6 p-3'>
           <Link to={article.slug}>
             <img src={article.image} />
             <h3>{article.title}</h3>
           </Link>
+          <Link to='#'>
+            <span className='category'>{article.category}</span>
+          </Link>
+          <br />
+          <div className='tags'>
+            {article.tags.map((tag) => (
+              <span className='tag' key={tag}>
+                {tag}
+              </span>
+            ))}
+          </div>
+          <p>{article.description}</p>
         </div>
       ))}
     </div>
@@ -22,28 +34,104 @@ const RecentArticles = ({ articles }) => (
 );
 
 RecentArticles.propTypes = {
-  articles: PropTypes.string.isRequired,
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+      date: PropTypes.string.isRequired,
+      updated: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      tags: PropTypes.arrayOf(PropTypes.string).isRequired,
+      topic: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
-
 const RenderRecentArticles = () => (
   <StaticQuery
     query={graphql`
       query RecentArticles {
-        articles: allMarkdownRemark(
-          sort: { order: DESC, fields: [frontmatter___updated] }
+        categories: allMarkdownRemark(
           filter: {
             frontmatter: {
               templateKey: { eq: "article" }
               published: { eq: true }
             }
           }
-          limit: 4
+          limit: 10
         ) {
           edges {
             node {
-              excerpt
+              frontmatter {
+                category
+              }
+            }
+          }
+        }
+        tags: allMarkdownRemark(
+          filter: {
+            frontmatter: {
+              templateKey: { eq: "article" }
+              published: { eq: true }
+            }
+          }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              frontmatter {
+                tags
+              }
+            }
+          }
+        }
+        articles: allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: {
+            frontmatter: {
+              templateKey: { eq: "article" }
+              published: { eq: true }
+            }
+          }
+          limit: 1000
+        ) {
+          edges {
+            node {
               fields {
                 slug
+                # readingTime {
+                #   text
+                # }
+              }
+              frontmatter {
+                title
+                date
+                updated
+                description
+                tags
+                category
+                image
+              }
+            }
+          }
+        }
+        posts: allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___date] }
+          filter: {
+            frontmatter: {
+              templateKey: { eq: "article" }
+              published: { eq: true }
+            }
+          }
+          limit: 6
+        ) {
+          edges {
+            node {
+              fields {
+                slug
+                # readingTime {
+                #   text
+                # }
               }
               frontmatter {
                 title
@@ -60,7 +148,6 @@ const RenderRecentArticles = () => (
     `}
     render={(data) => {
       return <RecentArticles articles={getArticlesFromQuery(data.articles)} />;
-      // return <p>{JSON.stringify(getArticlesFromQuery(data.articles))}</p>;
     }}
   />
 );
